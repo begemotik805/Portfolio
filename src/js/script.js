e84876196f454cdbb0dbd73d92d6e48596ba6f84
Your Name <you@example.com>
2022-06-07 19:23:03 +1000

    portfolio

--- src/js/script.js
+++ src/js/script.js
@@ -11,7 +11,12 @@ closeElem.addEventListener('click', () => {
     menu.classList.remove('active');
 });
 
+const counters = document.querySelectorAll('.skills__ratings-counter'),
+    lines = document.querySelectorAll('.skills__ratings-line span');
 
+counters.forEach((item, i) => {
+    lines[i].style.width = item.innerHTML;
+});
 
 
 
